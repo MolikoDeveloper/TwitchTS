21e0aa02d4ff74768268ee914c37da980ac6f33c
albertosaurus_ac <molikodev@gmail.com>
2024-02-16 19:38:19 -0300

    test require() failed.

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 declare module 'twitchts';
-
 import { EventSub } from './src/helix/EventSub/EventSub';
 import { IrcBase } from './src/irc/IrcBase';
 
 export {
     IrcBase as IrcClient,
     EventSub
-}
+} 
