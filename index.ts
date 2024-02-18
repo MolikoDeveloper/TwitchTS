825a6be677b6cbf8e177cd38f0b5435732ecaa75
albertosaurus_ac <molikodev@gmail.com>
2024-02-18 02:01:36 -0300

    exported Options type

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 declare module 'twitchts';
 import { EventSub } from './src/helix/EventSub/EventSub';
 import { IrcBase } from './src/irc/IrcBase';
+import { type Options } from './src/util/session';
 
 export {
     IrcBase as IrcClient,
     EventSub
-} 
+};
+export type { Options };
